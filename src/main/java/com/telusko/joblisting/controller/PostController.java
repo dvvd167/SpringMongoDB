fc614f58ba5780f117ecc71bb587b299c16efc68
dvvd167 <85768978+dvvd167@users.noreply.github.com>
2024-01-23 21:22:39 +0100

    Update HelloWorldController.java

--- src/main/java/com/telusko/joblisting/controller/PostController.java
+++ src/main/java/com/telusko/joblisting/controller/PostController.java
@@ -1,53 +1,14 @@
-package com.telusko.joblisting.controller;
+// Controller class
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import com.telusko.joblisting.repository.PostRepository;
-import com.telusko.joblisting.model.Post;
-import com.telusko.joblisting.repository.SearchRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
+@Controller
+public class HelloWorldController {
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
-
-@RestController
-@CrossOrigin(origins = "http://localhost:3000")
-public class PostController
-{
-
-    @Autowired
-    PostRepository repo;
-
-    @Autowired
-    SearchRepository srepo;
-
-    @ApiIgnore
-    @RequestMapping(value="/")
-    public void redirect(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/swagger-ui.html");
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "hello";
     }
-
-    @GetMapping("/allPosts")
-    @CrossOrigin
-    public List<Post> getAllPosts()
-    {
-        return repo.findAll();
-    }
-    // posts/java
-    @GetMapping("/posts/{text}")
-    @CrossOrigin
-    public List<Post> search(@PathVariable String text)
-    {
-        return srepo.findByText(text);
-    }
-
-    @PostMapping("/post")
-    @CrossOrigin
-    public Post addPost(@RequestBody Post post)
-    {
-        return repo.save(post);
-    }
-
-
 }
